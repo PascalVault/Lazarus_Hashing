b40664cb909d4c55b5381dcbc14ddfbcbb6a0683
Tom <118055911+PascalVault@users.noreply.github.com>
2022-11-27 02:37:05 +0100

    Add files via upload

---
+++ MD2.pas
@@ -0,0 +1,149 @@
+unit MD2;
+//MD-2
+//Author: domasz
+//Last Update: 2022-11-26
+//Licence: MIT
+
+interface
+
+uses SysUtils, HasherBase;
+
+type THasherMD2 = class(THasherbase)
+  private
+    checksum: array[0..15] of Byte;
+    work: array[0..47] of Byte;
+  public
+    constructor Create; override;
+    procedure Update(Msg: PByte; Length: Integer); override;
+    function Final: String; override;
+end;
+
+implementation
+
+const SBOX: array[0..255] of Byte = (
+$29, $2E, $43, $C9, $A2, $D8, $7C, $01, $3D, $36, $54, $A1,
+$EC, $F0, $06, $13, $62, $A7, $05, $F3, $C0, $C7, $73, $8C,
+$98, $93, $2B, $D9, $BC, $4C, $82, $CA, $1E, $9B, $57, $3C,
+$FD, $D4, $E0, $16, $67, $42, $6F, $18, $8A, $17, $E5, $12,
+$BE, $4E, $C4, $D6, $DA, $9E, $DE, $49, $A0, $FB, $F5, $8E,
+$BB, $2F, $EE, $7A, $A9, $68, $79, $91, $15, $B2, $07, $3F,
+$94, $C2, $10, $89, $0B, $22, $5F, $21, $80, $7F, $5D, $9A,
+$5A, $90, $32, $27, $35, $3E, $CC, $E7, $BF, $F7, $97, $03,
+$FF, $19, $30, $B3, $48, $A5, $B5, $D1, $D7, $5E, $92, $2A,
+$AC, $56, $AA, $C6, $4F, $B8, $38, $D2, $96, $A4, $7D, $B6,
+$76, $FC, $6B, $E2, $9C, $74, $04, $F1, $45, $9D, $70, $59,
+$64, $71, $87, $20, $86, $5B, $CF, $65, $E6, $2D, $A8, $02,
+$1B, $60, $25, $AD, $AE, $B0, $B9, $F6, $1C, $46, $61, $69,
+$34, $40, $7E, $0F, $55, $47, $A3, $23, $DD, $51, $AF, $3A,
+$C3, $5C, $F9, $CE, $BA, $C5, $EA, $26, $2C, $53, $0D, $6E,
+$85, $28, $84, $09, $D3, $DF, $CD, $F4, $41, $81, $4D, $52,
+$6A, $DC, $37, $C8, $6C, $C1, $AB, $FA, $24, $E1, $7B, $08,
+$0C, $BD, $B1, $4A, $78, $88, $95, $8B, $E3, $63, $E8, $6D,
+$E9, $CB, $D5, $FE, $3B, $00, $1D, $39, $F2, $EF, $B7, $0E,
+$66, $58, $D0, $E4, $A6, $77, $72, $F8, $EB, $75, $4B, $0A,
+$31, $44, $50, $B4, $8F, $ED, $1F, $1A, $DB, $99, $8D, $33,
+$9F, $11, $83, $14);
+
+constructor THasherMD2.Create;
+var i: Integer;
+begin
+  inherited Create;
+  Check := '12BD4EFDD922B5C8C7B773F26EF4E35F';
+
+  for i:= 0 to 15 do begin
+    checksum[i] := 0;
+
+    work[(i * 3)    ] := 0;
+    work[(i * 3) + 1] := 0;
+    work[(i * 3) + 2] := 0;
+  end;
+end;
+
+procedure THasherMD2.Update(Msg: PByte; Length: Integer);
+var i,j,k: Integer;
+    T: Byte;
+    l: Byte;
+    b: Byte;
+    buf: array[0..15] of Byte;
+    Left: Integer;
+    Size: Integer;
+begin
+  i := 0;
+
+  while i < Length-1 do begin
+
+    if Length - i > 15 then begin
+      Move(Msg^, buf[0], 16);
+      Inc(Msg, 16);
+    end
+    else begin
+      Size := Length mod 16;
+
+      FillChar(Buf, 16, 0);
+      Move(Msg^, Buf[0], Size);
+      Inc(Msg, Size);
+    end;
+
+    Left := 16 - (length mod 16);
+    if (Left = 0) then Left := 16;
+
+    for j:=0 to Left-1 do begin
+      Buf[Length+j] := Left;
+    end;
+
+    l := checksum[15];
+    for k:=0 to 15 do begin
+      b := buf[k];
+
+      work[16 + k] := b;
+      work[(16 shl 1) + k] := (work[k] xor b);
+      l := (checksum[k] xor SBOX[(b xor l) and $FF]);
+      checksum[k] := l;
+    end;
+
+    t := 0;
+    for k:=0 to 17 do begin
+      for j:=0 to 47 do begin
+        t := (work[j] xor SBOX[t]); 
+        work[j] := t;
+      end;
+
+     t := (t + k);
+    end;
+
+    Inc(i, 16);
+  end;
+end;
+
+function THasherMD2.Final: String;
+var Res: array[0..15] of Byte;
+    t: Byte;
+    i,j: Integer;
+    b: Byte;
+begin
+
+  for i:=0 to 15 do begin
+    b := checksum[i];
+    work[16 + i] := b;
+    work[(16 shl 1) + i] := (work[i] xor b);
+  end;
+
+  t := 0;
+  for i:=0 to 17 do begin
+    for j:=0 to 47 do begin
+      t := (work[j] xor SBOX[t]); work[j] := t;
+    end;
+
+    t := t + i;
+  end;
+
+  for i:=0 to 15 do Res[i] := work[i];
+
+  Result := '';
+  for i:=0 to 15 do Result := Result + IntToHex(Res[i], 2);
+end;
+
+initialization
+  HasherList.RegisterHasher('MD-2', THasherMD2);
+
+end.
---
+++ MD4.pas
@@ -0,0 +1,202 @@
+unit MD4;
+//MD-4
+//Author: domasz
+//Last Update: 2022-11-27
+//Licence: MIT  
+
+interface
+
+uses SysUtils, HasherBase;
+
+type THasherMD4 = class(THasherbase)
+  private
+    Value: array[0..3] of Cardinal;
+  public
+    constructor Create; override;
+    procedure Update(Msg: PByte; Length: Integer); override;
+    function Final: String; override;
+end;
+
+implementation
+
+constructor THasherMD4.Create;
+begin
+  inherited Create;
+  Check := '2AE523785D0CAF4D2FB557C12016185C';
+
+  value[0] := $67452301;
+  value[1] := $EFCDAB89;
+  value[2] := $98BADCFE;
+  value[3] := $10325476;
+end;
+
+procedure THasherMD4.Update(Msg: PByte; Length: Integer);
+var i: Integer;
+    j: Integer;
+    A,B,C,D: Cardinal;
+    X: array[0..15] of Cardinal;
+    buf: array[0..63] of Byte;
+    Bits: Int64;
+    Left: Integer;
+begin
+  j := 0;
+
+  while j < Length-1 do begin
+
+    if Length - j > 63 then begin
+      Move(Msg^, buf[0], 64);
+      Inc(Msg, 64);
+    end
+    else begin
+      Left := Length mod 64;
+
+      FillChar(Buf, 64, 0);
+      Move(Msg^, buf[0], Left);
+      Inc(Msg, Left);
+
+      Buf[Left] := $80;
+
+      Bits := Length shl 3;
+
+      buf[56] := bits;
+      buf[57] := bits shr 8;
+      buf[58] := bits shr 16;
+      buf[59] := bits shr 24;
+      buf[60] := bits shr 32;
+      buf[61] := bits shr 40;
+      buf[62] := bits shr 48;
+      Buf[63] := bits shr 56;
+    end;
+
+    for i:=0 to 15 do begin
+      X[i] := (buf[i*4]) or (buf[i*4 +1]) shl 8 or (buf[i*4 +2]) shl 16 or (buf[i*4 +3]) shl 24;
+    end;
+
+    A := value[0];
+    B := value[1];
+    C := value[2];
+    D := value[3];
+
+    A += ((B and C) or ((not B) and D)) + X[0];
+    A := RolDword(A, 3);
+    D += ((A and B) or ((not A) and C)) + X[1];
+    D := RolDword(D, 7);
+    C += ((D and A) or ((not D) and B)) + X[2];
+    C := RolDword(C, 11);
+    B += ((C and D) or ((not C) and A)) + X[3];
+    B := RolDword(B, 19);
+    A += ((B and C) or ((not B) and D)) + X[4];
+    A := RolDword(A, 3);
+    D += ((A and B) or ((not A) and C)) + X[5];
+    D := RolDword(D, 7);
+    C += ((D and A) or ((not D) and B)) + X[6];
+    C := RolDword(C, 11);
+    B += ((C and D) or ((not C) and A)) + X[7];
+    B := RolDword(B, 19);
+    A += ((B and C) or ((not B) and D)) + X[8];
+    A := RolDword(A, 3);
+    D += ((A and B) or ((not A) and C)) + X[9];
+    D := RolDword(D, 7);
+    C += ((D and A) or ((not D) and B)) + X[10];
+    C := RolDword(C, 11);
+    B += ((C and D) or ((not C) and A)) + X[11];
+    B := RolDword(B, 19);
+    A += ((B and C) or ((not B) and D)) + X[12];
+    A := RolDword(A, 3);
+    D += ((A and B) or ((not A) and C)) + X[13];
+    D := RolDword(D, 7);
+    C += ((D and A) or ((not D) and B)) + X[14];
+    C := RolDword(C, 11);
+    B += ((C and D) or ((not C) and A)) + X[15];
+    B := RolDword(B, 19);
+
+    A += ((B and (C or D)) or (C and D)) + X[0] + $5A827999;
+    A := RolDword(A, 3);
+    D += ((A and (B or C)) or (B and C)) + X[4] + $5A827999;
+    D := RolDword(D, 5);
+    C += ((D and (A or B)) or (A and B)) + X[8] + $5A827999;
+    C := RolDword(C, 9);
+    B += ((C and (D or A)) or (D and A)) + X[12] + $5A827999;
+    B := RolDword(B, 13);
+    A += ((B and (C or D)) or (C and D)) + X[1] + $5A827999;
+    A := RolDword(A, 3);
+    D += ((A and (B or C)) or (B and C)) + X[5] + $5A827999;
+    D := RolDword(D, 5);
+    C += ((D and (A or B)) or (A and B)) + X[9] + $5A827999;
+    C := RolDword(C, 9);
+    B += ((C and (D or A)) or (D and A)) + X[13] + $5A827999;
+    B := RolDword(B, 13);
+    A += ((B and (C or D)) or (C and D)) + X[2] + $5A827999;
+    A := RolDword(A, 3);
+    D += ((A and (B or C)) or (B and C)) + X[6] + $5A827999;
+    D := RolDword(D, 5);
+    C += ((D and (A or B)) or (A and B)) + X[10] + $5A827999;
+    C := RolDword(C, 9);
+    B += ((C and (D or A)) or (D and A)) + X[14] + $5A827999;
+    B := RolDword(B, 13);
+    A += ((B and (C or D)) or (C and D)) + X[3] + $5A827999;
+    A := RolDword(A, 3);
+    D += ((A and (B or C)) or (B and C)) + X[7] + $5A827999;
+    D := RolDword(D, 5);
+    C += ((D and (A or B)) or (A and B)) + X[11] + $5A827999;
+    C := RolDword(C, 9);
+    B += ((C and (D or A)) or (D and A)) + X[15] + $5A827999;
+    B := RolDword(B, 13);
+
+    A += (B xor C xor D) + X[0] + $6ED9EBA1;
+    A := RolDword(A, 3);
+    D += (A xor B xor C) + X[8] + $6ED9EBA1;
+    D := RolDword(D, 9);
+    C += (D xor A xor B) + X[4] + $6ED9EBA1;
+    C := RolDword(C, 11);
+    B += (C xor D xor A) + X[12] + $6ED9EBA1;
+    B := RolDword(B, 15);
+    A += (B xor C xor D) + X[2] + $6ED9EBA1;
+    A := RolDword(A, 3);
+    D += (A xor B xor C) + X[10] + $6ED9EBA1;
+    D := RolDword(D, 9);
+    C += (D xor A xor B) + X[6] + $6ED9EBA1;
+    C := RolDword(C, 11);
+    B += (C xor D xor A) + X[14] + $6ED9EBA1;
+    B := RolDword(B, 15);
+    A += (B xor C xor D) + X[1] + $6ED9EBA1;
+    A := RolDword(A, 3);
+    D += (A xor B xor C) + X[9] + $6ED9EBA1;
+    D := RolDword(D, 9);
+    C += (D xor A xor B) + X[5] + $6ED9EBA1;
+    C := RolDword(C, 11);
+    B += (C xor D xor A) + X[13] + $6ED9EBA1;
+    B := RolDword(B, 15);
+    A += (B xor C xor D) + X[3] + $6ED9EBA1;
+    A := RolDword(A, 3);
+    D += (A xor B xor C) + X[11] + $6ED9EBA1;
+    D := RolDword(D, 9);
+    C += (D xor A xor B) + X[7] + $6ED9EBA1;
+    C := RolDword(C, 11);
+    B += (C xor D xor A) + X[15] + $6ED9EBA1;
+    B := RolDword(B, 15);
+
+    value[0] := value[0] + A;
+    value[1] := value[1] + B;
+    value[2] := value[2] + C;
+    value[3] := value[3] + D;
+
+    Inc(j, 64);
+  end;
+end;
+
+function THasherMD4.Final: String;
+begin
+  Value[0] := SwapEndian(Value[0]);
+  Value[1] := SwapEndian(Value[1]);
+  Value[2] := SwapEndian(Value[2]);
+  Value[3] := SwapEndian(Value[3]);
+
+  Result := IntToHex(Value[0], 8) + IntToHex(Value[1], 8) + IntToHex(Value[2], 8) +
+            IntToHex(Value[3], 8);
+end;
+
+initialization
+  HasherList.RegisterHasher('MD-4', THasherMD4);
+
+end.
